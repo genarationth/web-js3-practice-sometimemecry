6952a6b487033e44fd2c89b9fb235f27a70e9ed2
sometimemecry <songphon.yy@hotmail.com>
2023-03-13 16:20:26 +0700

    Exercise 2

---
+++ script1.js
@@ -0,0 +1,5 @@
+const myInfo = (firstname,location,hobby) => {
+    return console.log("Hi, my name is " + firstname + ". I live in " + location + " and enjoy" + hobby);
+}
+
+myInfo('Songphon','Buriram','Coding');
