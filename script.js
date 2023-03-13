3ae73bc11fd9c7f2d83cd2f5f4f397fe96029974
sometimemecry <songphon.yy@hotmail.com>
2023-03-13 16:16:33 +0700

    Exercise 1

---
+++ script.js
@@ -0,0 +1,19 @@
+// const addFunction = (a,b) => {
+//     return a + b;
+// }
+// console.log(addFunction(1,2));
+
+// const addFunction = (a,b) => {
+//     return a - b;
+// }
+// console.log(addFunction(1,2));
+
+// const addFunction = (a,b) => {
+//     return a * b;
+// }
+// console.log(addFunction(1,2));
+
+const addFunction = (a,b) => {
+    return a / b;
+}
+console.log(addFunction(1,2));
